826baecc0895274c2bae04caacb278404bfded67
NotNullBool <106617184+NotNullBool@users.noreply.github.com>
2023-09-26 11:07:14 -0400

    Remove trunk

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use bevy::prelude::*;
 fn main() {
-    App::new().add_systems(Update, hello_world).run();
+    App::new()
+        .add_systems(Update, hello_world)
+        .run();
 }
 
 fn hello_world() {
